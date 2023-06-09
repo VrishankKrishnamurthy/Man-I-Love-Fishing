449e476a799a126fc97b1fa6d6f24323a8140f60
VrishankKrishnamurthy <vrishankk@g.ucla.edu>
2023-06-09 12:14:16 -0700

    Add start/enter to control panel

--- fishing.js
+++ fishing.js
@@ -128,13 +128,14 @@ export class Fishing extends Scene {
     
     make_control_panel()
     {
+        this.key_triggered_button("Start", ["Enter"], () => {});
         this.key_triggered_button("Increase Speed", ["ArrowUp"], () => {});
         this.key_triggered_button("Decrease Speed", ["ArrowDown"], () => {});
         this.key_triggered_button("Rotate Left", ["ArrowLeft"], () => {});
         this.key_triggered_button("Rotate Right", ["ArrowRight"], () => {});
         this.key_triggered_button("Boat View", ["b"], () => this.view = "Boat");
         this.key_triggered_button("Top View", ["t"], () => this.view = "Top");
-        this.key_triggered_button("Free View", ["g"], () => this.view = "Free"); // testing only
+        this.key_triggered_button("Free View", ["g"], () => this.view = "Free");
         this.key_triggered_button("Toggle Fishing Radius", ["h"], () => this.show_radius = !this.show_radius);
     }
 
